997f17755da988c2e9737c96f18a8aaf4e95404e
sosanbutt9 <sosanbutt.s@gmail.com>
2026-05-04 22:18:28 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:3000",
+    "https://photoshare-fe-chi.vercel.app"
 ]
 CORS_ALLOW_CREDENTIALS = True
 
