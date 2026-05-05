ae2ae4992a3b795cbec25367050621431d7cec52
sosanbutt9 <sosanbutt.s@gmail.com>
2026-05-05 22:49:45 +0500

    Adjust middleware and CORS configuration
    Reorder middleware and update CORS settings.

--- config/settings.py
+++ config/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -125,16 +125,19 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://127.0.0.1:3000",
     "https://photoshare-fe-chi.vercel.app",
-    "https://photofe-gsdmghf0chhfbrc3.italynorth-01.azurewebsites.net"
+    "https://photofe-gsdmghf0chhfbrc3.italynorth-01.azurewebsites.net",
+    "http://photofe-gsdmghf0chhfbrc3.italynorth-01.azurewebsites.net",
 ]
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:3000",
     "https://photoshare-fe-chi.vercel.app",
-    "https://photofe-gsdmghf0chhfbrc3.italynorth-01.azurewebsites.net"
+    "https://photofe-gsdmghf0chhfbrc3.italynorth-01.azurewebsites.net",
+    "http://photofe-gsdmghf0chhfbrc3.italynorth-01.azurewebsites.net",
 ]
 
 CACHES = {
