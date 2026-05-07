2d39a9fbd800cf1a0ada28eb1e7a0e3377349794
sosanbutt9 <sosanbutt.s@gmail.com>
2026-05-07 08:22:55 +0500

    Change database name path in settings.py

--- config/settings.py
+++ config/settings.py
@@ -69,7 +69,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'NAME': '/home/data/db.sqlite3',
     }
 }
 
